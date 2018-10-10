7e0e5b9be0ee3a7f821f71fece46c8cfda1937da
Savannah Niemeyer <savannahniemeyer@gmail.com>
2018-10-10 09:09:06 -0500

    context reducer

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Contacts from './components/contacts/Contacts';
+import Header from './components/layout/Header';
+import { Provider } from './context';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <Contacts />
+          </div>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/context.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    default:
+      return state;
+  }
+};
+//making global state
+export class Provider extends Component {
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@gmail.com',
+        phone: '555-555-5555'
+      },
+      {
+        id: 2,
+        name: 'Henry Joe',
+        email: 'hjoe@gmail.com',
+        phone: '455-445-5544'
+      },
+      {
+        id: 3,
+        name: 'Sarah Doohe',
+        email: 'saddd@gmail.com',
+        phone: '533-555-5535'
+      }
+    ],
+    dispatch: action => this.setState(state => reducer(state, action))
+  };
+
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
